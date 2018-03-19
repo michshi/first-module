58cbc6980f4b64a63c86fd8d005132139c8e0407
shieldsy123 <michael.t.shields@gmail.com>
2018-03-19 10:52:36 -0600

    sphere volume working

--- geometry.js
+++ geometry.js
@@ -1,7 +1,5 @@
-// Geometry stuff
+// With node, require there to be this file, inferred to be .js, that has this module exports, assign the value of module exports and put it in to this const name
+const volume = require('./volume')
 
-function torusVolume(majorRadius, minorRadius) {
-  return (Math.PI * Math.pow(minorRadius, 2)) * (2 * Math.PI * majorRadius)
-}
-
-console.log(torusVolume(4,3));
+console.log(volume.torusVolume(4, 3))
+console.log(volume.sphereVolume(1))
