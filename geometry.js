6ea538dfba30bc076fff740cc8203a046d150325
shieldsy123 <michael.t.shields@gmail.com>
2018-03-19 11:03:11 -0600

    torus and sphere volumes working

--- geometry.js
+++ geometry.js
@@ -1,5 +1,6 @@
-// With node, require there to be this file, inferred to be .js, that has this module exports, assign the value of module exports and put it in to this const name
-const volume = require('./volume')
+// With node, require there to be this file, inferred to be .js, that has this module exports, assign the value of module exports and put it in to this const name. The destructing syntax below says to node to find these keys in the object and grabs the keys which assigns those keys to the function
+const {sphereVolume, torusVolume } = require('./volume')
 
-console.log(volume.torusVolume(4, 3))
-console.log(volume.sphereVolume(1))
+console.log(torusVolume(4, 3))
+
+console.log(sphereVolume(1))
